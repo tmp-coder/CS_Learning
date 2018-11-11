345c47e3022e02ad1c428e1e7a7272617724b603
zt zou <zouzhitao_cq@163.com>
2018-11-11 23:53:23 +0800

    Adversarial Search for 2048 game

---
+++ CSMM101AI/project/2048-game/PlayerAI_3.py
@@ -0,0 +1,150 @@
+from random import randint
+
+from BaseAI_3 import BaseAI
+from Displayer_3 import Displayer
+import time
+from Grid_3 import directionVectors
+import math
+
+# computerAi para
+tileVal = [2, 4]  # tile value
+grid_size = 4
+
+displayer = Displayer()  # debug
+
+
+class PlayerAI(BaseAI):
+
+    def getMove(self, grid):
+        solver = AlphaBetaSolver(heuristics_fun)
+        return solver.solve(grid)
+
+
+class AlphaBetaSolver():
+
+    def __init__(self, estimateFun, max_turn=256, maxTime=0.1):
+        # self.upper_utility = upper_utility  # estimate max tile value
+        # self.prev_time = time()
+        # self.maxTime = search_time
+        self.max_dep = max_turn
+        self.estimateFun = estimateFun
+        self.time = time.clock()
+        self.maxTime = maxTime
+
+    def solve(self, grid):
+        m = self.maximize(grid, -math.inf, math.inf)[0]
+
+        if m is None:
+            return grid.getAvailableMoves()[0]
+        return m
+
+    def terminal_test(self, actions, dep):
+        return len(actions) == 0 or time.clock() - self.time > self.maxTime or dep > self.max_dep
+
+    def minimize(self, grid, alpha, beta, dep=0):
+        # self.dep +=1
+        cells = grid.getAvailableCells()
+
+        if self.terminal_test(cells, dep):
+            return self.estimateFun(grid)
+
+        min_utility = math.inf
+
+        for cell in cells:
+            child = grid.clone()  # grid is not change
+
+            # for val in tileVal:
+            child.setCellValue(cell, tileVal[0])
+            utility = self.maximize(child, alpha, beta, dep + 1)[1]
+            # print("maximize utility = ", utility)
+            if utility < min_utility:
+                min_utility = utility
+
+            if min_utility <= alpha:  # pruned
+                return min_utility
+
+            if min_utility < beta:
+                beta = min_utility
+
+        return min_utility
+
+    def maximize(self, grid, alpha, beta, dep=0):
+        # self.dep +=1
+        moves = grid.getAvailableMoves()
+
+        if self.terminal_test(moves, dep):
+            return (None, self.estimateFun(grid))
+
+        max_utility = -1
+        mov = None
+        for m in moves:
+
+            child = grid.clone()
+            if not child.move(m):
+                continue
+
+            utility = self.minimize(child, alpha, beta, dep + 1)
+            # print("minimize utility = ", utility)
+            if utility > max_utility:
+                max_utility = utility
+                mov = m
+
+            if max_utility >= beta:
+                break
+
+            if max_utility > alpha:
+                alpha = max_utility
+
+        return (mov, max_utility)
+
+
+# some helper function
+
+# some selected para
+
+max_power = 20
+
+weight = [1024] + [4 ** i for i in range(max_power)]
+
+# estimate function
+def heuristics_fun(grid):
+    return estimate_score(grid) - 4 * penalty(grid)
+
+
+def estimate_score(grid, weight=weight, max_power=max_power):
+    cnt = [0 for i in range(max_power + 1)]
+
+    for i in range(grid_size):
+        for j in range(grid_size):
+            idx = int(math.log2(grid.getCellValue((i, j)) + 0.0000001) + 0.5)
+
+            if idx < 0:
+                idx = 0
+
+            cnt[idx] += 1
+
+    ret = 0
+    for i in range(len(cnt)):
+        ret += cnt[i] * weight[i]
+
+    return ret
+
+
+def penalty(grid):
+    ret = 0
+    for i in range(grid_size):
+        for j in range(grid_size):
+
+            cur_pos = (i, j)
+            val = grid.getCellValue(cur_pos)
+            if val == 0:
+                continue
+            for dir in directionVectors:
+                pos = (cur_pos[0] + dir[0], cur_pos[1] + dir[1])
+
+                if grid.crossBound(pos):
+                    continue
+
+                ret += abs(val - grid.getCellValue(pos))
+
+    return ret
---
+++ CSMM101AI/project/2048-game/README.md
@@ -0,0 +1,13 @@
+2048 game 
+
+CSMM101 AI Week 4 Project: Adversarial Search and Games
+
+link : [Week 4 Project: Adversarial Search and Games](https://courses.edx.org/courses/course-v1:ColumbiaX+CSMM.101x+3T2018/courseware/b640307843b6433d9d8ace7c0a0eaefc/6366aea9fddd43b8b7db9cd658be2cb1/1?activate_block_id=block-v1%3AColumbiaX%2BCSMM.101x%2B3T2018%2Btype%40vertical%2Bblock%4045c14c7196554a69a7358171cf85d810)
+
+my trick:
+
+1. alpha - beta pruning
+2. feature 1 : weight_vec * val_vec
+3. feature 2 : difference between two adjacent tiles
+
+p.s. this trick get 1024
